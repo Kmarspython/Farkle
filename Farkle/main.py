534c52043a52236159952f59fc976f84a1752a4d
Kmarspython <kaimars@me.com>
2023-01-16 21:52:47 -0800

    Added more to the main.py file, see Update file

--- Farkle/main.py
+++ Farkle/main.py
@@ -2,6 +2,8 @@ import pygame as p
 
 WIDTH = HEIGHT = 600
 DICE_SIZE = HEIGHT // 13
+BUTTON_HEIGHT = 50
+BUTTON_WIDTH = 100
 MAX_FPS = 15
 IMAGES = {}
 
@@ -14,11 +16,15 @@ def load_images():
 def main():
     """The main loop for the program to run in"""
     p.init()
+    p.font.init()
+    myfont = p.font.SysFont("Comic Sans MS", 30)
     running = True
     screen = p.display.set_mode((WIDTH, HEIGHT))
     screen.fill(p.Color("Light Blue"))
     clock = p.time.Clock()
     board = ["d1", "d2", "d3", "d4", "d5"]
+    roll_color = p.Color("Green")
+    keep_color = p.Color("Green")
     load_images()
     while running:
         for e in p.event.get():
@@ -31,8 +37,16 @@ def main():
                 elif e.key == p.K_n:
                     if len(board) != 0:
                         board.pop()
+                elif e.key == p.K_h:
+                    roll_color = p.Color("Gray") if roll_color == p.Color("Green") else p.Color("Green")
         screen.fill(p.Color("Light Blue"))
         draw_dice(board, screen)
+        roll, roll_text = create_button(screen, roll_color, BUTTON_WIDTH, BUTTON_HEIGHT,
+                      (WIDTH // 2) - BUTTON_WIDTH // 2 - BUTTON_WIDTH * .75, (HEIGHT // 2) - BUTTON_HEIGHT // 2 - 25,
+                      myfont, "Roll", p.Color("Black"))
+        keep, keep_text = create_button(screen, keep_color, BUTTON_WIDTH, BUTTON_HEIGHT,
+                      (WIDTH // 2) - BUTTON_WIDTH // 2 + BUTTON_WIDTH * .75, (HEIGHT // 2) - BUTTON_HEIGHT // 2 - 25,
+                      myfont, "Keep", p.Color("Black"))
         clock.tick(MAX_FPS)
         p.display.flip()
 
@@ -46,6 +60,11 @@ def draw_dice(board, screen):
         x = ((2 * r + 4) * DICE_SIZE) + (DICE_SIZE * onetotwo[x1]) if r <= 2 else ((2 * (r - 3) + 4) * DICE_SIZE) +(DICE_SIZE * onetotwo[len(board) - 3])
         p.draw.rect(screen, p.Color("Light Blue"), p.Rect((x, y, DICE_SIZE, DICE_SIZE)))
         screen.blit(IMAGES[dice], p.Rect((x, y, DICE_SIZE, DICE_SIZE)))
+
+def create_button(screen, button_color, button_width, button_height, button_x, button_y, font, text, text_color):
+    font_surf = font.render(text, False, text_color)
+    font_width, font_height = font_surf.get_size()
+    return p.draw.rect(screen, button_color, [button_x, button_y, button_width, BUTTON_HEIGHT]), screen.blit(font_surf, ((button_x + (button_width - font_width) / 2), button_y + (button_height - font_height) / 2))
 
 if __name__ == "__main__":
     main()
--- Update.md
+++ Update.md
@@ -1,2 +1,5 @@
 1/15/2023
 Started the project. Created the loop for the program to run in. Got dice sprites. The dice display on the screen with anywhere from 1 to 6 dice. About 3 hours.
+
+1/16/2023
+Started adding "roll" and "keep" buttons. About an hour and a half.
